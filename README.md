fffa23d8eea9f8dfef37a64a9e5111bfd8e58b7c
hoXyy <hoxtf2@outlook.com>
2021-05-29 14:27:49 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# simple-spotify-twitch-bot
+# Simple Spotify Twitch Bot
 Simple Twitch bot that displays the currently playing song on a specific Spotify account.
+![Example](https://i.imgur.com/Dst1zrb.png)
+
+# Setup
+Clone the repository, run `npm install` and fill in the necessary information in the `config.json` file.
+
+### Config file
+The config file requires this info:
+* Spotify app client ID and secret (which you can get from the [Spotify Developer Site](https://developer.spotify.com/))
+* Username and OAuth token (which you can get [here](https://twitchapps.com/tmi/)) of the account you want to use for the bot
+* Name of the channel you want the bot account to join and post in
+* Command you want to use to trigger the bot
+
+The correct syntax of the config file is the following:
+```json
+{
+  "spotify": {
+    "client_id": "<Spotify client ID>",
+    "client_secret": "<Spotify client secret>",
+  },
+  "twitch": {
+    "username": "<Twitch username of the account you want to use for the bot>",
+    "channel": "<Channel you want the bot to join and post in>",
+    "oauth_token": "<OAuth token to log into the bot account>",
+    "command": "!song"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,179 @@
+const express = require('express');
+const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+const tmi = require('tmi.js');
+const jsonfile = require('jsonfile');
+const prettyms = require('pretty-ms');
+let config = jsonfile.readFileSync("./config.json");
+const opts = {
+    options: {
+        debug: false,
+    },
+    connection: { 
+        secure: true,
+        reconnect: true
+    },
+    identity: {
+        username: config.twitch.username,
+        password: config.twitch.oauth_token,
+    },
+    channels: [config.twitch.channel]
+};
+const twitchChat = new tmi.Client(opts);
+
+
+twitchChat.connect().then(() => {
+    console.log(`Logged into Twitch chat and connected to ${config.twitch.channel}`);
+});
+
+twitchChat.on('message', onMessageHandler);
+
+async function onMessageHandler(channel, tags, message, self)
+{
+    if (message.toLowerCase() === config.twitch.command) {
+        songstring = await FormatSongString();
+        twitchChat.say(channel, songstring);
+    }
+}
+
+const scopes = [
+    'user-read-playback-state',
+    "user-read-currently-playing"
+];
+
+const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:9822/callback/spotify',
+    clientId: config.spotify.client_id,
+    clientSecret: config.spotify.client_secret
+})
+
+
+
+app.get('/login/spotify', (req, res) => {
+    res.redirect(spotifyApi.createAuthorizeURL(scopes))
+});
+
+app.get('/callback/spotify', (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+  
+    if (error) {
+      console.error('Callback Error:', error);
+      res.send(`Callback Error: ${error}`);
+      return;
+    }
+  
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        const access_token = data.body['access_token'];
+        const refresh_token = data.body['refresh_token'];
+        const expires_in = data.body['expires_in'];
+        
+        config.spotify.access_token = access_token;
+        config.spotify.refresh_token = refresh_token;
+        config.spotify.token_expires = new Date().getTime() + (expires_in * 1000);
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.setRefreshToken(refresh_token);
+        jsonfile.writeFile("./config.json", config, { spaces: 2 });
+  
+        console.log(
+          `Sucessfully retrieved access token. Expires in ${expires_in} s.`
+        );
+        res.send('Success! You can now close the window.');
+        FormatSongString();
+  
+        setInterval(async () => {
+            RefreshSpotifyToken();
+        }, expires_in / 2 * 1000);
+      })
+      .catch(error => {
+        console.error('Error getting Tokens:', error);
+        res.send(`Error getting Tokens: ${error}`);
+      });
+  });
+
+app.listen(9822, () => {
+    if (!config.spotify.access_token)
+    {
+        console.log("No Spotify token found! Open http://localhost:9822/login/spotify to authenticate with Spotify.")
+    }
+    else
+    {
+        if ((new Date().getTime() - config.spotify.token_expires) > 3600000) // if token is expired, get new one
+        {
+            spotifyApi.setRefreshToken(config.spotify.refresh_token)
+            RefreshSpotifyToken();
+        }
+        else
+        {
+            spotifyApi.setAccessToken(config.spotify.access_token) // load tokens from config file and use it for calls
+            spotifyApi.setRefreshToken(config.spotify.refresh_token)
+        }
+        setInterval(async () => {
+            RefreshSpotifyToken();
+        }, 3600 / 2 * 1000);
+    }
+});
+
+
+async function RefreshSpotifyToken()
+{
+    const data = await spotifyApi.refreshAccessToken();
+    const access_token = data.body['access_token'];
+    const refresh_token = data.body['refresh_token'];
+
+    config.spotify.access_token = access_token;
+    config.spotify.refresh_token = refresh_token;
+    jsonfile.writeFile("./config.json", config);
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+}
+
+async function GetCurrentSong()
+{
+    var songinfo = {};
+    songinfo.artists = [];
+    const data = await spotifyApi.getMyCurrentPlaybackState().then(function(data) { return data; });
+    if (data.statusCode == 200)
+    {
+        data.body.item.artists.forEach(artist => {
+            songinfo.artists.push(artist.name);
+        });
+        songinfo.title = data.body.item.name;
+        songinfo.local = data.body.item.is_local;
+        songinfo.length = data.body.item.duration_ms;
+        songinfo.progress = data.body.progress_ms;
+        if (!songinfo.local)
+        {
+            songinfo.url = data.body.item.external_urls.spotify;
+        }
+        return songinfo;
+    }
+    else
+    {
+        return;
+    }
+};
+
+async function FormatSongString()
+{
+    const songinfo = await GetCurrentSong();
+    var songstring;
+    if (songinfo != undefined)
+    {
+        const progress = prettyms(songinfo.progress, { colonNotation: true, secondsDecimalDigits: 0});
+        const length = prettyms(songinfo.length, { colonNotation: true, secondsDecimalDigits: 0});
+        songstring = `Current song: ${songinfo.artists.join(', ')} - ${songinfo.title} (${progress}/${length})`;
+        if (songinfo.url) {
+            songstring += ` - ${songinfo.url}`
+        }
+    }
+    else
+    {
+        songstring = "No song currently playing (or unable to get song from Spotify)."
+    }
+    return songstring;
+}
+
